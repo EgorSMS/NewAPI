5ee36862ff8a681ea3860693d85c932420fb1d64
EgorSMS <negoric@bk.ru>
2020-12-04 16:45:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/newapi/ItemOfList.kt
@@ -0,0 +1,12 @@
+package com.example.newapi
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class ItemOfList (
+    var ImgSrc: String,
+    var ImgTitle: String,
+    var img_description: String,
+    var ImgDescription: String
+) : Parcelable
